cc887b8bc70ad275849abc10211172c5c0f29d8f
otonye77 <oamietubodie@gmail.com>
2023-05-15 08:19:57 +0100

    registeration

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,52 @@
-import createError from "http-errors";
-import express, { Request, Response, NextFunction } from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+import createError from 'http-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-import registerRouter from "./routes/register";
-import { connectDb, sequelize } from "./config/database";
+
+import registerRouter from './routes/register';
+import { connectDb, sequelize } from './config/database';
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
-
-
-app.use('/user', registerRouter)
-
-
-
-
+app.use('/user', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-//DB connection
-const syncDatabase = async()=> {
- await connectDb()
-  sequelize.sync({ force:false }).then(() => {
-  console.log('Database synced successfully')
-})
-}
 
-syncDatabase()
+
+//DB connection
+const syncDatabase = async () => {
+  await connectDb();
+  sequelize.sync({ force: false }).then(() => {
+    console.log('Database synced successfully');
+  });
+};
+
+syncDatabase();
 // error handler
-app.use(function (
-  err: createError.HttpError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+app.use(function (err: createError.HttpError, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 export default app;
--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,10 +1,11 @@
-import express, {Response, Request} from "express";
-import register from "../controller/register";
-import registerSchema from "../utils/registerValidation";
-const validator = require("express-joi-validation").createValidator({});
+import express, { Response, Request } from 'express';
+import register from '../controller/register';
+import registerSchema from '../utils/registerValidation';
+const validator = require('express-joi-validation').createValidator({});
 const router = express.Router();
 
+router.post('/register', validator.body(registerSchema), register);
 
-router.post("/register", validator.body(registerSchema), register);
+
 
 export default router;
