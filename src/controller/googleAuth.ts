fef51f07d7f81f61c8efa1fd9b06c48d7f5ab086
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2023-06-29 11:51:09 +0100

    Updating backend

--- src/controller/googleAuth.ts
+++ src/controller/googleAuth.ts
@@ -10,17 +10,17 @@ const port = 4000;
 const JWT_SECRET_KEY= process.env.JWT_SECRET_KEY as string
 
 
-export const googleSignIn = async (req:Request, res:Response) => {
-    try {
-        const code = req.query.code 
-  
+export const googleSignIn = async (req: Request, res: Response) => {
+  try {
+    const code = req.query.code;
+
     const { id_token, access_token } = await getTokens({
       code,
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       redirectUri: `http://localhost:${port}/auth/google`,
     });
-  
+
     // Fetch the user's profile with the access token and bearer
     const googleUser = await axios
       .get(
@@ -29,48 +29,42 @@ export const googleSignIn = async (req:Request, res:Response) => {
           headers: {
             Authorization: `Bearer ${id_token}`,
           },
-        }  
+        }
       )
       .then((res) => res.data)
       .catch((error) => {
         console.error(`Failed to fetch user`);
         throw new Error(error.message);
       });
-        
-     const user = await User.findOne({ where: { email:googleUser.email} });
-  
-    if(!user){
-    const newUser: UserAttributes = await User.create({
+
+    const user = await User.findOne({ where: { email: googleUser.email } });
+
+    if (!user) {
+      const newUser: UserAttributes = await User.create({
         id: googleUser.id,
         firstName: googleUser.given_name,
         lastName: googleUser.family_name,
-        email:googleUser.email,
-        mentalCondition:"",
-        country:"",
-        state:"" ,
-        password:"",
-        gender: "",    
-        otp:null,
+        email: googleUser.email,
+        mentalCondition: "",
+        country: "",
+        state: "",
+        password: "",
+        gender: "",
+        otp: null,
         otp_expiry: null,
-        blocked:[],
-        profilePhoto: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600',
-        // verify: true
-         });
-         
-     const token = jwt.sign({ id: newUser.id }, JWT_SECRET_KEY,{ expiresIn: '30d' });
-     res.cookie('token', token, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 });  
-    //  return res.status(200).json({ message: 'User registered successfully' });
-    res.redirect("http://localhost:5173/dashboard")
-     }
-  
-    } catch (error) {
-      console.log(error)
-        return res.status(500).json({ Error: 'Internal Server Error' });
+        blocked: [],
+        profilePhoto:
+          "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600",
+      });
 
+      const token = jwt.sign({ id: newUser.id }, JWT_SECRET_KEY, {
+        expiresIn: "30d",
+      });
+      res.cookie("token", token, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
+      res.redirect("https://mindconnect.netlify.app/dashboard");
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ Error: "Internal Server Error" });
   }
-
-
-
-
-  
+};
